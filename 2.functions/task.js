c8400632b52b936519f462e6f4ac9a6fa944c3a0
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-06-27 16:05:13 +0300

    added tasks 1-2-3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -23,22 +23,34 @@ function getArrayParams(arr) {
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  for ( let i = 0; i < arr.length; i++){
+sum += arr[i]}
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = func(arrOfArr[i]);
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++){
+  let funcResult = func(arrOfArr[i]);
+  if (funcResult > max){
+  funcResult = max}}
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, diff;
+  min = arr[0];
+  max = arr[0];
+  diff = Math.abs(max - min);
+
+ for (let i = 0; i < arr.length; i++){
+ if (arr[0] > max){
+ max = arr[0];}
+ if (arr[0] < min){
+ min = arr[0];}
+ }
+ return diff;
 }
