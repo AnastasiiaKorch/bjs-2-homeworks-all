7c44975fd7348eb2d383490622c53dc89d1148ae
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-06-27 15:04:23 +0300

    task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,21 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+  if (arr[0] > max) {
+  max = arr[0]
+  }
+  if (arr[0] < min) {
+  min = arr[0]
+  }
+  sum += arr[i];
+  }
+  avg = Number ((sum / arr.length).toFixed(2));
+
 
   return { min: min, max: max, avg: avg };
 }
