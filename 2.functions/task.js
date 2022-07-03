08419c9cd6f8c32aec021c4f907e337b750f0405
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-07-03 19:44:49 +0300

    added rework tasks 1-2-3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -5,52 +5,58 @@ function getArrayParams(arr) {
   max = arr[0];
   sum = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-  if (arr[0] > max) {
-  max = arr[0]
-  }
-  if (arr[0] < min) {
-  min = arr[0]
-  }
-  sum += arr[i];
-  }
-  avg = Number ((sum / arr.length).toFixed(2));
+    for (let i = 0; i < arr.length; i++)  {
+      if (arr[i] > max) {
+        max = arr[i]
+      }
 
+      if (arr[i] < min) {
+        min = arr[i]
+      }
+      sum += arr[i];
+    }
+      avg = Number((sum / arr.length).toFixed(2));
 
-  return { min: min, max: max, avg: avg };
+   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-  for ( let i = 0; i < arr.length; i++){
-sum += arr[i]}
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max = func(arrOfArr[i]);
+  let max = -Infinity;
 
-  for (let i = 0; i < arrOfArr.length; i++){
-  let funcResult = func(arrOfArr[i]);
-  if (funcResult > max){
-  funcResult = max}}
+  for (let y = 0; y < arrOfArr.length; y++) {
+  func(arrOfArr[y]);
+    if (max < func(arrOfArr[y])) {
+      max = func(arrOfArr[y])
+    }
+  }
 
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  let min, max, diff;
-  min = arr[0];
-  max = arr[0];
-  diff = Math.abs(max - min);
+  let max = -Infinity;
+  let min = Infinity;
 
- for (let i = 0; i < arr.length; i++){
- if (arr[0] > max){
- max = arr[0];}
- if (arr[0] < min){
- min = arr[0];}
- }
- return diff;
+   for (let i = 0; i < arr.length; i++) {
+     if (arr[i] > max){
+       max = arr[i];
+     }
+     if (arr[i] < min) {
+       min = arr[i];
+     }
+   }
+
+ return (Math.abs(max - min));
 }
