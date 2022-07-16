3b85bf0301eb0cbcc4c5deb7eaad1dff596b0035
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-07-16 19:49:00 +0300

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,52 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age
+}
+let student1 = new Student ("Tony", "male", 36);
+let student2 = new Student ("Dan", "male", 45);
 
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.addMark = function(mark){
+  if(this.marks === undefined){
+    this.marks = [];
+    this.marks.push(mark);
+  }
+  else {
+    this.marks.push(mark)
+  }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...theMarks) {
+  if(this.marks === undefined){
+    this.marks = theMarks;
+  }
+  else {
+    for (let i = 0; i < theMarks.length; i++) {
+      this.theMarks.push(theMarks[i]);
+    }
+  }
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < this.theMarks.length; i++){
+  sum += this.theMarks[i]};
+  }
+  return sum / this.theMarks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  this.exclude = reason;
+  delete this.subject;
+  delete this.marks;
+
+}
+let student3 = new Student("Tony", "male", 37);
+ student3.setSubject("Algebra");
+ student3.addMark(5);
+ student3.addMark(4);
+ student3.addMark(5);
+ console.log(student3);
