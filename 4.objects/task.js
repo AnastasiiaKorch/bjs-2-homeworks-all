f92e41fe899c9a8a402f57f1df0fa46250a5cae3
Anastasiia Korchagina <anastasia_92_92@list.ru>
2022-07-19 20:33:57 +0300

    rework task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -13,40 +13,25 @@ Student.prototype.setSubject = function(subjectName) {
 Student.prototype.addMark = function(mark){
   if(this.marks === undefined){
     this.marks = [];
-    this.marks.push(mark);
   }
-  else {
-    this.marks.push(mark)
-  }
+  this.marks.push(mark);
 }
 
 Student.prototype.addMarks = function(...theMarks) {
   if(this.marks === undefined){
-    this.marks = theMarks;
+    this.marks = [];
   }
-  else {
-    for (let i = 0; i < theMarks.length; i++) {
-      this.theMarks.push(theMarks[i]);
-    }
-  }
+theMarks.forEach(mark => this.marks.push(mark));
+}
 
 Student.prototype.getAverage = function () {
-  let sum = 0;
-  for (let i = 0; i < this.theMarks.length; i++){
-  sum += this.theMarks[i]};
-  }
-  return sum / this.theMarks.length;
+  let sum = this.marks.reduce((a, b) => a + b);
+  let average = sum / this.marks.length;
+  return average;
 }
 
 Student.prototype.exclude = function(reason) {
-  this.exclude = reason;
   delete this.subject;
   delete this.marks;
-
+  this.exclude = reason;
 }
-let student3 = new Student("Tony", "male", 37);
- student3.setSubject("Algebra");
- student3.addMark(5);
- student3.addMark(4);
- student3.addMark(5);
- console.log(student3);
